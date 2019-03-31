f2b1a3ecfcb2357ddde8e41ea4e4d53d6e462efd
Kristi Pina <kpina@worcester.edu>
2019-03-30 21:42:10 -0400

    search-bar component created

--- ampath-simple-app/src/app/app.module.ts
+++ ampath-simple-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule
