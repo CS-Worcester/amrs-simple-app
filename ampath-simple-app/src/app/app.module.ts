b33d6c53d6dad8361b1816e3d7032a2104c33f56
Gulshan Kumar <gkumar@worcester.edu>
2019-04-02 12:24:45 -0400

    Questionnare Form Component Setup

--- ampath-simple-app/src/app/app.module.ts
+++ ampath-simple-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuestionnaireFormComponent } from './questionnaire-form/questionnaire-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionnaireFormComponent
   ],
   imports: [
     BrowserModule
---
+++ ampath-simple-app/src/app/questionnaire-form/questionnaire-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-questionnaire-form',
+  templateUrl: './questionnaire-form.component.html',
+  styleUrls: ['./questionnaire-form.component.css']
+})
+export class QuestionnaireFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
