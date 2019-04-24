281830c0ccf62cf54cc92c651be07462958fe357
Karl R. Wurst <karl@w-sts.com>
2019-04-24 12:52:02 -0400

    Revert "Left nav bar"

--- ampath-simple-app/src/app/app.module.ts
+++ ampath-simple-app/src/app/app.module.ts
@@ -2,12 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
@@ -17,13 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-	AppRoutingModule,
     MatToolbarModule,
-	BrowserAnimationsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatSidenavModule,
-    MatToolbarModule
+    BrowserAnimationsModule
 
   ],
   providers: [],
