5a2620035d805c7dc3a23b98c21bbd39b78be4c7
Mia Rogers <mia@therogers.net>
2019-04-25 19:57:21 -0400

    Forms and Fields working

--- ampath-simple-app/src/app/app.module.ts
+++ ampath-simple-app/src/app/app.module.ts
@@ -1,22 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { FormsModule} from '@angular/forms';
+import {MatFormFieldModule, MatTabsModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import {MatInputModule} from '@angular/material';
+import {MatSelectModule} from '@angular/material';
+import { MatCheckboxModule} from '@angular/material';
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { ReactiveFormsModule} from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    TabsComponent
+],
   imports: [
     BrowserModule,
+    FormsModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
     MatToolbarModule,
-    BrowserAnimationsModule
-
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
-  providers: [],
+  providers: [
+    MatNativeDateModule,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
